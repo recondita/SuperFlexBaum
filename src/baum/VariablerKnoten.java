3c6ae681b88ebb077bc736e8a831c6d63c8a1499
jan <jan.hofmeier@yahoo.de>
2014-07-14 14:32:13 +0200

    Randnummer fertig?

--- src/baum/VariablerKnoten.java
+++ src/baum/VariablerKnoten.java
@@ -9,59 +9,54 @@ import org.apache.lucene.document.Document;
  * 
  * @author Jan Hofmeier
  *
- * @param <T> String/Int
- * @param <E> Enum das die moeglichen Attribute enthaelt.
+ * @param <T>
+ *            String/Int
+ * @param <E>
+ *            Enum das die moeglichen Attribute enthaelt.
  */
-public class VariablerKnoten<E extends Enum<E>&Ordner> extends Knoten<E>
-{
+public class VariablerKnoten<E extends Enum<E> & Ordner> extends Knoten<E> {
 
 	protected Ordner[] suchFelder;
 	protected int hauptSuchFeld;
 	private TreeMap<String, BaumTeil> kinder;
 
-	protected VariablerKnoten(String name, BaumStruktur<E> struktur)
-	{
+	protected VariablerKnoten(String name, BaumStruktur<E> struktur) {
 		super(name, struktur);
 		suchFelder = struktur.getSuchFelder();
-		hauptSuchFeld=struktur.getHauptSuchFeld();
+		hauptSuchFeld = struktur.getHauptSuchFeld();
 		kinder = new TreeMap<String, BaumTeil>();
 	}
 
 	@Override
-	public BaumTeil[] gebeKinder()
-	{
+	public BaumTeil[] gebeKinder() {
 		Collection<BaumTeil> temp = kinder.values();
 		return temp.toArray(new BaumTeil[temp.size()]);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected boolean add(Document doc)
-	{
+	protected boolean add(Document doc) {
 		String[] temp = gebeFeld(doc);
 		boolean ret = false;
-		if (temp != null)
-		{
+		if (temp != null) {
 			// String[] destlist = temp.split(", ");
-			for (int i=0; i< temp.length;i++)
-			{
+			for (int i = 0; i < temp.length; i++) {
 				Knoten<E> weiter = null;
-				if(temp[i]==null)
+				if (temp[i] == null)
 					continue;
-				if (!kinder.containsKey(temp[i]))
-				{
-					String name=gebeName(doc,i);
-					if (struktur.kinder.length > 0)
-					{
-						weiter = machSubKnoten(name);
+				if (!kinder.containsKey(temp[i])) {
+					//String name = gebeName(doc, i);
+					if (struktur.kinder.length > 0) {
+						weiter = machSubKnoten(temp[i]);
 						kinder.put(temp[i], weiter);
-					} else
-					{
-						kinder.put(temp[i], new Blatt(name, doc.get(struktur.getValueFlield().getFeld())));
+					} else {
+						kinder.put(
+								temp[i],
+								new Blatt(temp[i], doc.get(struktur
+										.getValueFlield().getFeld())));
 						ret = true;
 					}
-				} else
-				{
+				} else {
 					if (struktur.kinder.length > 0)
 						weiter = (Knoten<E>) kinder.get(temp[i]);
 					else
@@ -77,35 +72,28 @@ public class VariablerKnoten<E extends Enum<E>&Ordner> extends Knoten<E>
 		return ret;
 	}
 
-	private Knoten<E> machSubKnoten(String name)
-	{
+	private Knoten<E> machSubKnoten(String name) {
 		if (struktur.kinder[0].statisch)
 			return new StatischerKnoten<E>(name + "", struktur.kinder[0]);
-		else 
+		else
 			return new VariablerKnoten<E>(name + "", struktur.kinder[0]);
 	}
 
 	@Override
-	protected void remove(Document doc)
-	{
+	protected void remove(Document doc) {
 		String temp = doc.get(suchFelder[hauptSuchFeld].getFeld());
-		if (temp != null && temp.length() > 0 && !temp.equals("-1"))
-		{
+		if (temp != null && temp.length() > 0 && !temp.equals("-1")) {
 			String[] destlist = temp.split(", ");
-			for (String dest : destlist)
-			{
-				if (kinder.containsKey(dest))
-				{
-					if (struktur.kinder.length > 0)
-					{
+			for (String dest : destlist) {
+				if (kinder.containsKey(dest)) {
+					if (struktur.kinder.length > 0) {
 						@SuppressWarnings("unchecked")
 						Knoten<E> next = ((Knoten<E>) kinder.get(dest));
 						next.remove(doc);
 						if (next.isEmpty())
 							kinder.remove(dest);
 
-					} else
-					{
+					} else {
 						kinder.remove(dest);
 					}
 				}
@@ -115,55 +103,120 @@ public class VariablerKnoten<E extends Enum<E>&Ordner> extends Knoten<E>
 		}
 	}
 
-	public boolean isEmpty()
-	{
+	public boolean isEmpty() {
 		return kinder.isEmpty();
 	}
 
 	@Override
-	public boolean contains(Document doc)
-	{
-		String temp = doc.get(suchFelder[hauptSuchFeld].getFeld());
-		if (temp != null && temp.length() > 0 && !temp.equals("-1"))
-		{
-			String[] destlist = temp.split(", ");
-			for (String dest : destlist)
-			{
-				if (kinder.containsKey(dest))
-				{
-					if (struktur.kinder.length > 0)
-					{
-						@SuppressWarnings("unchecked")
-						Knoten<E> next = ((Knoten<E>) kinder.get(dest));
-						if (next.contains(doc))
-							return true;
+	public boolean contains(Document doc) {
+		/*
+		 * String temp = doc.get(suchFelder[hauptSuchFeld].getFeld()); if (temp
+		 * != null && temp.length() > 0 && !temp.equals("-1")) {
+		 */
+		String[] destlist = gebeFeld(doc); // temp.split(", ");
+		for (String dest : destlist) {
+			if (kinder.containsKey(dest)) {
+				if (struktur.kinder.length > 0) {
+					@SuppressWarnings("unchecked")
+					Knoten<E> next = ((Knoten<E>) kinder.get(dest));
+					if (next.contains(doc))
+						return true;
 
-					} else
-					{
-						if (kinder.containsKey(dest))
-							return true;
-					}
+				} else {
+					if (kinder.containsKey(dest))
+						return true;
 				}
-
 			}
 
 		}
+
 		return false;
 	}
 
-	protected String gebeName(Document doc, int nummer)
-	{
-		StringBuffer ret=new StringBuffer();
-		for(int i=0; i<suchFelder.length; i++)
+	/*
+	 * protected String gebeName(Document doc, int suchfeld) { StringBuffer
+	 * ret=new StringBuffer(); for(int i=0; i<suchFelder.length; i++) {
+	 * ret.append
+	 * (doc.getValues(suchFelder[i].getFeld())[(suchfeld==hauptSuchFeld
+	 * )?suchfeld:0]); ret.append(" "); } return ret.toString(); }
+	 */
+
+	protected String[] gebeFeld(Document doc) {
+		String[] ret = doc.getValues(suchFelder[hauptSuchFeld].getFeld());
+		if (suchFelder.length == 1)
 		{
-			ret.append(doc.getValues(suchFelder[i].getFeld())[(nummer==hauptSuchFeld)?nummer:0]);
-			ret.append(" ");
+			if(suchFelder[hauptSuchFeld].isInt())
+			{
+				for(int i=0; i<ret.length;i++)
+					ret[i]=vorLeer(ret[i],suchFelder[hauptSuchFeld].maxStellen());
+			}
+			return ret;
 		}
-		return ret.toString();
+
+		String pre;
+		if (hauptSuchFeld > 0) {
+			StringBuffer preSB = new StringBuffer();
+			for (int i = 0; i < hauptSuchFeld; i++) {
+				String[] temp = doc.getValues(suchFelder[i].getFeld());
+				for (String val : temp) {
+					preSB.append(suchFelder[i].isInt()?vorLeer(val,suchFelder[i].maxStellen()):val);
+					preSB.append(" ");
+				}
+			}
+			pre = preSB.toString();
+		} else {
+			if(suchFelder[hauptSuchFeld].isInt())
+			{
+				for(int i=0; i<ret.length;i++)
+					ret[i]=vorLeer(ret[i],suchFelder[hauptSuchFeld].maxStellen());
+			}
+			pre = "";
+		}
+
+		String suff;
+		if (suchFelder.length > hauptSuchFeld + 1) {
+			StringBuffer suffSB = new StringBuffer();
+			for (int i = hauptSuchFeld + 1; i < suchFelder.length; i++) {
+				String[] temp = doc.getValues(suchFelder[i].getFeld());
+				for (String val : temp) {
+					suffSB.append(val);
+					suffSB.append(" ");
+				}
+
+			}
+			suff = suffSB.toString();
+		} else {
+			suff = "";
+		}
+
+		for (int i = 0; i < ret.length; i++) {
+			StringBuffer temp = new StringBuffer();
+			temp.append(pre);
+			temp.append(ret[i]);
+			if (!suff.equals("")) {
+				temp.append(" ");
+				temp.append(suff);
+			}
+			ret[i]=temp.toString();
+		}
+		return ret;
 	}
 	
-	protected String[] gebeFeld(Document doc)
+	
+	protected String vorLeer(String str, int length)
 	{
-		return doc.getValues(suchFelder[hauptSuchFeld].getFeld());
+		if(str.length()==length)
+			return str;
+		for(int i=0; i<str.length(); i++)
+			if(str.charAt(i)<0x30||str.charAt(i)>0x39)
+				return str;
+		
+		StringBuffer ret=new StringBuffer();
+		for(int i=str.length(); i<length;i++)
+		{
+			ret.append(" ");
+		}
+		ret.append(str);
+		return ret.toString();
 	}
 }
